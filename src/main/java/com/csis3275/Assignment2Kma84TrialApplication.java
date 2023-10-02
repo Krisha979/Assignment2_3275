163ef7ed2d6de149294d4075995eaa502436718c
Krisha Mahat <krishamahat751@gmail.com>
2023-10-02 11:24:33 -0400

    Added files

---
+++ src/main/java/com/csis3275/Assignment2Kma84TrialApplication.java
@@ -0,0 +1,13 @@
+package com.csis3275;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Assignment2Kma84TrialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Assignment2Kma84TrialApplication.class, args);
+	}
+
+}
